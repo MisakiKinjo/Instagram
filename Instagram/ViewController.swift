9c5647064494f90fc576b4c5400b18ed5f64d159
Misaki Kinjo <misaki0827bitodemi@gmail.com>
2022-03-29 10:48:42 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 金城美咲 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
