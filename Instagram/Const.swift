cb5290f8ff6b7436efa6f5b1af82978674629e33
Misaki Kinjo <misaki0827bitodemi@gmail.com>
2022-04-08 22:34:40 +0900

    コメントボタンを追加しました

---
+++ Instagram/Const.swift
@@ -0,0 +1,27 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 金城美咲 on 2022/04/03.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+    static let CommentPath = "comments"
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
